aac6c54e0dcbe66d0afd936f0fdb8430ea14af85
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-12-14 19:03:55 +0900

    retry after 1s

--- app/go/app_handlers.go
+++ app/go/app_handlers.go
@@ -680,7 +680,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE user_id = ? ORDER BY created_at DESC LIMIT 1`, user.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &appGetNotificationResponse{
-				RetryAfterMs: 30,
+				RetryAfterMs: 1000,
 			})
 			return
 		}
@@ -727,7 +727,7 @@ func appGetNotification(w http.ResponseWriter, r *http.Request) {
 			CreatedAt: ride.CreatedAt.UnixMilli(),
 			UpdateAt:  ride.UpdatedAt.UnixMilli(),
 		},
-		RetryAfterMs: 30,
+		RetryAfterMs: 1000,
 	}
 
 	if ride.ChairID.Valid {
